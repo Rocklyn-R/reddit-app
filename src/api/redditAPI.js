792ed7de6f9795cdb81274e2b66dfcadf050c0a5
Rocklyn-R <antica.rusi@gmail.com>
2024-01-23 12:00:31 +0100

    Implement caching

--- src/api/redditAPI.js
+++ src/api/redditAPI.js
@@ -1,81 +1,53 @@
 const baseUrl = "https://www.reddit.com";
 
-export const getSubredditPosts = async (subreddit) => {
+const cache = {};
+const CACHE_EXPIRY_MS = 4 * 60 * 60 * 1000; //4 hours in miliseconds
 
+export const getSubredditPosts = async (subreddit) => {
+    if (cache[subreddit] && cache[subreddit].timestamp + CACHE_EXPIRY_MS > Date.now()) {
+        return cache[subreddit].data;
+    }
     const response = await fetch(`${baseUrl}${subreddit}.json`)
     const json = await response.json();
     const newArray = json.data.children.map(post => post.data);
+    cache[subreddit] = { data: newArray, timestamp: Date.now() };
     return newArray;
 }
-
-
-
-
-
 
 export const getSubreddits = async () => {
+    const cacheKey = "subreddits";
+    if (cache[cacheKey] && cache[cacheKey].timestamp + CACHE_EXPIRY_MS > Date.now()) {
+        return cache[cacheKey].data
+    }
     const response = await fetch(`${baseUrl}/subreddits.json`);
     const json = await response.json();
-    const newArray = json.data.children.map(subreddit => subreddit.data);
+    const newArray = json.data.children.map(subreddit => subreddit.data)
+    cache[cacheKey] = { data: newArray, timestamp: Date.now() };
     return newArray;
 }
 
-/*export const getSubredditPosts = async (subreddit) => {
-    return new Promise((resolve, reject) => {
-        switch (subreddit) {
-            case 'pets':
-                resolve(pets);
-                break;
-            case 'fitness':
-                resolve(fitness);
-                break;
-            case 'music':
-                resolve(music);
-                break;
-            case 'party':
-                resolve(party);
-                break;
-            default:
-                resolve(party);
-                break;
-        }
-    });
-};*/
-
-/*export const getPostComments = async (permalink) => {
-    return new Promise((resolve, reject) => {
-
-        switch (permalink) {
-            case 'petsComments':
-                resolve(petsComments);
-                break;
-            case 'fitnessComments':
-                resolve(fitnessComments);
-                break;
-            case 'musicComments':
-                resolve(musicComments);
-                break;
-            case 'partyComments':
-                resolve(partyComments);
-                break;
-            default:
-                resolve('none of these');
-                break;
-        }
-    })
-
-}*/
 
 export const getPostComments = async (permalink) => {
+    if (cache[permalink] && cache[permalink].timestamp + CACHE_EXPIRY_MS > Date.now()) {
+        return cache[permalink].data;
+    }
+
     const response = await fetch(`${baseUrl}${permalink}.json`);
     const json = await response.json();
     const newArray = json[1].data.children
         .filter(comment => comment.data.body)
         .map(comment => comment.data);
+    cache[permalink] = { data: newArray, timestamp: Date.now() };
     return newArray;
 }
 
 export const getUserIcons = async (user) => {
+    // Check if the response is already cached
+    if (cache[user] && cache[user].timestamp + CACHE_EXPIRY_MS > Date.now()) {
+        console.log(cache[user]);
+        return cache[user].data;
+    }
+
     const emptyIconObject = {
         img_icon: "",
         snoovatar: ""
@@ -92,6 +64,10 @@ export const getUserIcons = async (user) => {
             img_icon: item.icon_img ? item.icon_img : "",
             snoovatar: item.snoovatar_img || "",
         }));
+
+        // Cache the response for this user
+        cache[user] = { data: updatedArray[0], timestamp: Date.now() };
+
         return updatedArray[0];
     } catch (error) {
         console.error(`Error fetching user icons for user: ${user}`, error);
